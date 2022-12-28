e014ba31792606c3ac52415e069dab8d263a13c2
DOM CHAROENYOS <dome@tel.co.th>
2022-12-29 00:34:48 +0700

    Update init-script.sh

--- test/jbc/init-script.sh
+++ test/jbc/init-script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ ! -d /datadir/geth/chaindata ]; then
     echo "/datadir/geth/chaindata not found, running 'geth init'..."
-    geth --datadir /datadir/geth  init /datadir/genesis.json
+    geth --datadir /datadir  init /datadir/genesis.json
     echo "...done!"
 fi
 
